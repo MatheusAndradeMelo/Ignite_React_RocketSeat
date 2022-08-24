d0ce93d18daf3e3a2856888fbc35cb6eaab32979
MatheusAndradeMelo <mandrademelo77@gmail.com>
2022-08-23 22:55:17 -0300

    feat: updates in header component, and create sidebar component, install phosphor library

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { useState } from "react";
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+import "./styles.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
-      <Post
-        author="Matheus Andrade"
-        content="lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      />
+      <Header />
 
-      <Post author="Albert Einstein" 
-      content="lorem ipsum dolor sit amet" 
-      />
-      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+          <Post
+            author="Matheus Andrade"
+            content="lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+
+          <Post author="Albert Einstein" content="lorem ipsum dolor sit amet" />
+        </main>
+      </div>
     </>
   );
 }
